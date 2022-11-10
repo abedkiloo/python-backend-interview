69ffe67e0d3a785c8b05cee04e611d5ad24c012d
ABEDNEGO.WAMBUA <ABEDNEGO.WAMBUA>
2022-11-10 12:50:40 +0300

    Updated documentation and updated tests for trips

--- amitruck_api/settings.py
+++ amitruck_api/settings.py
@@ -115,6 +115,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/minute',
+        'user': '1/minute'
+    }
 }
 
 # Internationalization
